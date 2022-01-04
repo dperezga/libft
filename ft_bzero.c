7638d16a319b314fdf74421d7dacfc7bf6b44b31
Denis Perez <dperez@fu-r1-p2.s19.be>
2022-01-04 21:26:43 +0100

    ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,24 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dperez <dperez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/03 18:21:50 by dperez            #+#    #+#             */
-/*   Updated: 2022/01/04 21:23:19 by dperez           ###   ########.fr       */
+/*   Created: 2022/01/04 20:44:25 by dperez            #+#    #+#             */
+/*   Updated: 2022/01/04 21:25:54 by dperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include<stdlib.h>
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*temp;
 
-size_t	ft_strlen(const char *s);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-
-#endif
+	temp = s;
+	if (n == 0)
+		return ;
+	while (n > 0)
+	{
+		*temp = 0;
+		s++;
+		n--;
+	}
+}
