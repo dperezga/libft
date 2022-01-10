97b6bd5cb35548c705d2ff5b5e3fd6480457c4b2
Denis <denispgua98@gmail.com>
2022-01-11 05:25:27 +0800

    im trying

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dperez <dperez@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 20:53:52 by dperez            #+#    #+#             */
-/*   Updated: 2022/01/10 21:27:10 by dperez           ###   ########.fr       */
+/*   Updated: 2022/01/11 05:01:45 by dperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 
 	len_dst = ft_strlen(dst);
 	len_src = ft_strlen(src);
+	if (dstsize == 0)
+		return (len_src);
+	if (dstsize < len_dst)
+		return (len_src);
+	ft_memmove(dst + len_dst, src, dstsize - len_dst - 1);
 	if (dstsize < len_dst + len_src)
-	{
-		ft_memmove(dst + len_dst, src, dstsize - len_dst - 1);
-		return (len_src + len_dst);
-	}
-	ft_memmove(dst + len_dst, src, len_src - 1);
+		ft_memset(dst + dstsize - 1, 0, 1);
 	return (len_src + len_dst);
 }
