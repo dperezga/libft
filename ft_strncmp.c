9d4534f22fa7e10fcb655e637ec5b7213f0e4d2b
Denis <denispgua98@gmail.com>
2022-01-11 08:37:14 +0800

    strncmp finito

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dperez <dperez@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 01:09:54 by dperez            #+#    #+#             */
-/*   Updated: 2022/01/11 01:18:00 by dperez           ###   ########.fr       */
+/*   Updated: 2022/01/11 08:29:15 by dperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (*s1 && *s2 && n > 0)
+	while (n > 0)
 	{
 		if (*s1 != *s2)
-			return (*s1 - *s2);
+			return ((unsigned char)*s1 - (unsigned char)*s2);
 		s1++;
 		s2++;
 		n--;
