adac8ffb4e37ffb232bfeecd6ce083b67909c707
Denis <denispgua98@gmail.com>
2022-01-12 22:10:29 +0800

    ft_strnstr is oki

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dperez <dperez@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 14:18:35 by dperez            #+#    #+#             */
-/*   Updated: 2022/01/12 14:28:17 by dperez           ###   ########.fr       */
+/*   Updated: 2022/01/12 22:09:27 by dperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	const char	*tmp_haystack;
+	size_t		needle_len;
+	size_t		counter;
 
+	counter = 0;
+	needle_len = ft_strlen(needle);
 	tmp_haystack = haystack;
 	if (ft_strlen(needle) == 0)
 		return ((char *)haystack);
 	while (*tmp_haystack)
 	{
-		if (ft_strncmp(tmp_haystack, needle, len) == 0)
+		if (counter + needle_len >= len)
+			return (NULL);
+		if (ft_strncmp(tmp_haystack, needle, needle_len) == 0)
 			return ((char *)tmp_haystack);
+		counter++;
 		tmp_haystack++;
 	}
 	return (NULL);
