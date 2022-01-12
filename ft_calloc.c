54746a37bd93ef72e9d549fe3444c15c1534fb29
Denis Perez <dperez@shi-r10-p6.s19.be>
2022-01-12 17:12:28 +0100

    ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dperez <dperez@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 17:05:40 by dperez            #+#    #+#             */
+/*   Updated: 2022/01/12 17:09:56 by dperez           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*allocated_mem;
+
+	allocated_mem = malloc(count * size);
+	if (allocated_mem == NULL)
+		return (NULL);
+	ft_bzero(allocated_mem, size);
+	return (allocated_mem);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dperez <dperez@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 18:21:50 by dperez            #+#    #+#             */
-/*   Updated: 2022/01/12 15:33:28 by dperez           ###   ########.fr       */
+/*   Updated: 2022/01/12 17:06:24 by dperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,5 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
 #endif
