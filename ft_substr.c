5b4571dd3dc806f74c819b6bace4b3fedbc4ead2
Denis <denispgua98@gmail.com>
2022-01-13 02:25:06 +0800

    Lets go

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dperez <dperez@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 18:34:07 by dperez            #+#    #+#             */
-/*   Updated: 2022/01/12 18:43:05 by dperez           ###   ########.fr       */
+/*   Updated: 2022/01/13 02:18:21 by dperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub_s;
 
-	sub_s = (char *)calloc(len, sizeof(sub_s));
+	sub_s = (char *)calloc(len + 1, sizeof(sub_s));
 	if (sub_s == NULL)
 		return (NULL);
-	ft_strlcpy(sub_s, s + start, len);
+	if (start > ft_strlen(s))
+		return ("");
+	ft_strlcpy(sub_s, s + start, len + 1);
 	return (sub_s);
 }
