0514218b36c29c0f3bd7547d4e707421d10071b1
Denis <denispgua98@gmail.com>
2022-01-15 04:04:52 +0800

    OPTIMIZATION ig, i just made the code shorter more like less redundant

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dperez <dperez@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 20:53:52 by dperez            #+#    #+#             */
-/*   Updated: 2022/01/11 06:18:49 by dperez           ###   ########.fr       */
+/*   Updated: 2022/01/15 03:53:47 by dperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 
 	len_dst = ft_strlen(dst);
 	len_src = ft_strlen(src);
-	if (dstsize == 0)
-		return (len_src);
 	if (dstsize <= len_dst)
 		return (len_src + dstsize);
 	ft_memmove(dst + len_dst, src, dstsize - len_dst - 1);
