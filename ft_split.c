54844182681ca23d4faf90c5d37e4c1d9a6e1cb1
Denis <denispgua98@gmail.com>
2022-01-28 17:21:07 +0100

    ft_split

--- ft_split.c
+++ ft_split.c
@@ -11,21 +11,62 @@
 /* ************************************************************************** */
 
 #include"libft.h"
+#include<stdio.h>
+
+static size_t	ft_len(char const *s, char c)
+{
+	size_t len;
+
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	return (len);
+}
 
 static size_t	ft_chunks(char const *s, char c)
 {
 	size_t	nb_chunks;
-	
+	char *tmp_s;
+
 	nb_chunks = 0;
-	while (*s != '\0')
+	tmp_s = (char *)s;
+	if (*tmp_s != c)
 	{
-		if (*s == c && *s + 1 != c)
-			nb_chunks;
-		s++;
+		nb_chunks++;
+		tmp_s++;
 	}
+	while (*tmp_s != '\0')
+	{
+		if (*tmp_s == c && *(tmp_s + 1) != c && *(tmp_s + 1))
+			nb_chunks++;
+		tmp_s++;
+	}
+	return (nb_chunks);
 }
 
 char	**ft_split(char const *s, char c)
 {
-		
+	printf("%s has %ld words\n", s, ft_chunks(s, c));
+	char	**splitted;
+	char	*tmp_s;
+	size_t	nb_words;
+	size_t	k;
+
+	nb_words = ft_chunks(s, c);
+	splitted = (char **)malloc(nb_words * sizeof(char *));
+	k = 0;
+	tmp_s = (char *)s;
+	while (*tmp_s && *tmp_s == c)
+		tmp_s++;
+	while (tmp_s != NULL && nb_words - k > 0 && *tmp_s)
+	{
+		splitted[k] = (char *)malloc(ft_len(tmp_s, c) * sizeof(char));
+		ft_memmove(splitted[k], tmp_s, ft_len(tmp_s, c));
+		printf("*s: %c and len: %ld\n", *tmp_s, ft_len(tmp_s, c));
+		tmp_s = ft_strchr(tmp_s, c);
+		while (tmp_s != NULL && *tmp_s && *tmp_s == c)
+			tmp_s++;
+		k++;
+	}
+	return (splitted);	
 }
