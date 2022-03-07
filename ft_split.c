6d95423362bc909e0e7f5c02b4346a4d98e392e5
dperez <dperez@student.s19.be>
2022-03-07 01:10:45 +0100

    ft_split done hehe

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dperez <dperez@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 06:47:52 by dperez            #+#    #+#             */
-/*   Updated: 2022/03/03 21:44:10 by dperez           ###   ########.fr       */
+/*   Updated: 2022/03/07 00:57:53 by dperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,27 @@ static size_t	ft_len(char const *s, char c)
 	return (len);
 }
 
+static char	**ft_free_all(char **splitted)
+{
+	int	i;
+
+	i = 0;
+	while (splitted[i])
+	{
+		free (splitted[i]);
+		i++;
+	}
+	free (splitted);
+	return (NULL);
+}
+
 static size_t	ft_chunks(char const *s, char c)
 {
 	size_t	nb_chunks;
 	char	*tmp_s;
 
+	if (!*s)
+		return (0);
 	nb_chunks = 0;
 	tmp_s = (char *)s;
 	if (*tmp_s != c)
@@ -44,6 +60,20 @@ static size_t	ft_chunks(char const *s, char c)
 	return (nb_chunks);
 }
 
+static void	ft_next_word(char **tmp_s, char c, int choice)
+{
+	if (choice == 0)
+	{
+		while (**tmp_s && **tmp_s == c)
+			(*tmp_s)++;
+	}
+	if (choice == 1)
+	{
+		while (**tmp_s && **tmp_s != c)
+			(*tmp_s)++;
+	}
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**splitted;
@@ -51,24 +81,24 @@ char	**ft_split(char const *s, char c)
 	size_t	nb_words;
 	size_t	k;
 
+	if (!s)
+		return (NULL);
 	nb_words = ft_chunks(s, c);
-	splitted = (char **)malloc(nb_words * sizeof(char *));
+	splitted = (char **)malloc((nb_words + 1) * sizeof(char *));
 	if (splitted == NULL)
 		return (NULL);
 	k = 0;
 	tmp_s = (char *)s;
-	while (*tmp_s && *tmp_s == c)
-		tmp_s++;
-	while (tmp_s != NULL && nb_words - k > 0 && *tmp_s)
+	while (k < nb_words)
 	{
-		splitted[k] = (char *)malloc((ft_len(tmp_s, c) + 1) * sizeof(char));
+		ft_next_word(&tmp_s, c, 0);
+		splitted[k] = (char *)malloc(sizeof(char) * (ft_len(tmp_s, c) + 1));
 		if (splitted[k] == NULL)
-			return (NULL);
+			return (ft_free_all(splitted));
 		ft_strlcpy(splitted[k], tmp_s, ft_len(tmp_s, c) + 1);
-		tmp_s = ft_strchr(tmp_s, c);
-		while (tmp_s != NULL && *tmp_s && *tmp_s == c)
-			tmp_s++;
+		ft_next_word(&tmp_s, c, 1);
 		k++;
 	}
+	splitted[k] = NULL;
 	return (splitted);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,27 @@
 /*   By: dperez <dperez@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 19:25:45 by dperez            #+#    #+#             */
-/*   Updated: 2022/03/06 21:10:31 by dperez           ###   ########.fr       */
+/*   Updated: 2022/03/07 01:09:09 by dperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
+
+static void	ft_extreme(char **begin, char **end, char const *set)
+{
+	while (**begin)
+	{
+		if (ft_strchr(set, **begin) == NULL)
+			break ;
+		(*begin)++;
+	}
+	while (**end)
+	{
+		if (ft_strchr(set, **end) == NULL)
+			break ;
+		(*end)--;
+	}
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -24,21 +40,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (ft_strdup(s1));
 	begin = (char *)s1;
 	end = begin + ft_strlen(s1) - 1;
-	while (*begin)
-	{
-		if (ft_strchr(set, *begin) == NULL)
-			break;
-        begin++;
-	}
-	while (*end)
-	{
-		if (ft_strchr(set, *end) == NULL)
-            break;
-		end--;
-	}
-    if (begin >= end)
-        return (ft_strdup("")); 
-    trimmed_str = (char *)ft_calloc(end - begin + 2, sizeof(char));
+	ft_extreme(&begin, &end, set);
+	if (begin >= end)
+		return (ft_strdup(""));
+	trimmed_str = (char *)ft_calloc(end - begin + 2, sizeof(char));
 	if (trimmed_str == NULL)
 		return (NULL);
 	ft_strlcpy(trimmed_str, begin, (end - begin) + 2);
